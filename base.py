74fe55e5c83f84ba9d423d895a9c26b4cef135ab
C.Grimal <kagrimal@live.com>
2021-02-02 21:14:22 +0100

    ajouts et moyenne
    - Modification de la structure Donnees pour permettre la création de nouvelles banques à partir de données lues
    
    - Corrections d'erreurs d'indentation
    ajout d'une méthode pour sortir toutes les images d'un chiffre donné
    
    -Implémentation d'un calcul de moyenne dans runner.py

--- base.py
+++ base.py
@@ -26,7 +26,7 @@ class Donnees():
     Par défaut, la division est de 80%.
     """
 
-    def __init__ (self, chemin = "mnist-original.mat", portion = 0.8):
+    def __init__ (self, images, labels, portion = 0.8):
         """
         Un set de données contient huit propriétés:
         deux jeux d'images qu'il contient, divisés selon un pourcentage en paramètre;
@@ -36,8 +36,6 @@ class Donnees():
         Si le pourcentage est paramétré à 100 ou 0, le jeu A sera le seul rempli.
         Par défaut, le chemin correspond au dossier de l'executable
         """
-        
-        images, labels = lecture(chemin)
         card_defaut = len(labels[0])
         
         if portion > 1 or portion < 0:
@@ -47,19 +45,21 @@ class Donnees():
             self.imagesA = images
             self.categA = labels[0]
             self.cardinalA = card_defaut
-            self.imagesB = np.zeroes(1)
-            self.categB = np.zeroes(1)
+            self.imagesB = np.zeros(1)
+            self.categB = np.zeros(1)
             self.cardinalB = 1
+            self.indexA = labels[0]
+            self.indexB = np.zeros(1)
 
-        if portion > 0 and portion < 1:
+        if portion > 0 and portion < 1: #En commentaires, des notes sur la forme de chaque variable
             indices = np.random.permutation(images.shape[0])
             setA_indx, setB_indx = indices[:int(portion*card_defaut)], indices[int(portion*card_defaut):]
-            self.indexA = setA_indx
-            self.indexB = setB_indx
-            self.imagesA = images[setA_indx,:]
-            self.imagesB = images[setB_indx,:]
-            self.categA = labels[:,setA_indx]
-            self.categB = labels[:,setB_indx]
+            self.indexA = setA_indx #matrice numpy de forme (portion*card_defaut,) (la virgule n'est pas accidentelle!)
+            self.indexB = setB_indx #contiennent les "anciens" indices des images, dans l'ordre de leur nouvelle base
+            self.imagesA = images[setA_indx,:] #matrice numpy de forme (portion*card_defaut, 784) 
+            self.imagesB = images[setB_indx,:] #contiennent les images, dans le nouvel ordre, sous forme de vecteurs de R784
+            self.categA = labels[:,setA_indx] #matrice numpy de forme (1, portion*card_defaut)
+            self.categB = labels[:,setB_indx] #ignorez le premier index, l'important est que le second vecteur fait correspondre les images à leur valeur, au même index
             self.cardinalA = int(card_defaut*portion)
             self.cardinalB = int(card_defaut*(1-portion))
             
@@ -93,9 +93,9 @@ class Donnees():
         renvoie l'index originel d'une image i dans une BD spécifiée
         """
         if bD == 'A':
-            return indexA[i]
+            return self.indexA[i]
         if bD == 'B':
-            return indexB[i]
+            return self.indexB[i]
         else:
             raise MauvaiseBD
 
@@ -115,6 +115,15 @@ class Donnees():
         if bD != 'A' and bD != 'B':
             raise MauvaiseBD
 
+    def affichage_index_bD(self, i, bD):
+        """
+        affiche l'image numérotée i dans la base de données spécifiée
+        """
+        if bD != 'A' and bD != 'B':
+            raise MauvaiseBD
+        self.affichage(i, bD)
+        
+
     def sortir_image(self, i):
         """
         renvoie l'image numérotée i dans la base originelle
@@ -124,4 +133,43 @@ class Donnees():
             return self.imagesA[iC]
         if bD == 'B':
             return self.imagesB[iC]
+
+    def sortir_chiffre(self, n, bD = 'A'):
+        """
+        Renvoie toutes les images correspondant au chiffre indiqué, dans une des bases
+        Par défaut, la base A est utilisée
+        Les images renvoyées sont toujours sous le même format numpy
+        """
+        if n < 0 or n > 9:
+            raise MauvaisChiffre
+        if bD == 'A':
+            index_temp = np.where(self.categA[0] == n)[0]
+            images_temp = np.zeros((len(index_temp), 784))
+            j = 0
+            for i in index_temp:
+                images_temp[j] = self.imagesA[i]
+                j+=1
+            return images_temp
+
+        if bD == 'B':
+            index_temp = np.where(self.categB[0] == n)[0]
+            images_temp = np.zeros((len(index_temp), 784))
+            j = 0
+            for i in index_temp:
+                images_temp[j] = self.imagesB[i]
+                j+=1
+            return images_temp
+
+        else:
+            raise MauvaiseBD
+
+
+
+
+
+
+
+
+
+        
 
